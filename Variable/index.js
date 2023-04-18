133ff40b272e6c56e1c1f5b0aa947461c33c29a7
unknown <tirenipaul@gmail.com>
2023-04-18 02:42:03 +0100

    My first commit

---
+++ Variable/index.js
@@ -0,0 +1,12 @@
+var myName;
+let myAge;
+
+myName = "Tireni"
+myAge = 22;
+
+var myHeight = "5ft"
+myHeight = 5.6;
+
+console.log("My name is " + myName);
+console.log("My age is " + myAge);
+console.log("My height is " + myHeight);
