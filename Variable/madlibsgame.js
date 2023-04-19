3b91c9aeb22f10428a8165bd57282c8ad9ba9005
unknown <tirenipaul@gmail.com>
2023-04-19 17:36:53 +0100

    My first commit

---
+++ Variable/madlibsgame.js
@@ -0,0 +1,10 @@
+console.log("Complete the poem with the right words.");
+let answer1 = "red";
+let answer2 = "blue";
+let answer3 = "sweet";
+let answer4 = "you";
+
+console.log("Roses are " + answer1);
+console.log("Violets are " + answer2);
+console.log("Sugar is " + answer3);
+console.log("And so are " + answer4);
